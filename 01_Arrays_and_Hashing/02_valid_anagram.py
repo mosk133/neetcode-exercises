4baf3b7c885356e6c77587e7a9801bef7ce15f9f
Guillem Martin Garcia <guillem.martin@mataro.epiaedu.cat>
2025-05-11 00:03:56 +0200

    refactor: improve solution for 02_valid_anagram in 01_Arrays_and_Hashing

--- 01_Arrays_and_Hashing/02_valid_anagram.py
+++ 01_Arrays_and_Hashing/02_valid_anagram.py
@@ -1,17 +1,18 @@
-# s = "racecar" 
-# t = "carrace"
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        str1 = sorted(s)
+        str2 = sorted(t)
+        if str1 == str2:
+            return True
+        return False
+
+s = "racecar" 
+t = "carrace"
+
+sol = Solution()
+print(sol.isAnagram(s, t))
 
 s = "jar" 
 t = "jam"
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        str1_sorted = sorted(s)
-        str1 = "".join(sorted(str1_sorted))
-        str2_sorted = sorted(t)
-        str2 = "".join(sorted(str2_sorted))
-        if str1 == str2:
-            return True
-
-sol = Solution()
 print(sol.isAnagram(s, t))
